467b045eea0cfc646f4f811f1eb422efc88857f3
amin ansari <135967143+amin1139@users.noreply.github.com>
2026-06-25 15:40:53 +0530

    Add import for musicplayer R resource in SplashActivity

--- app/src/main/java/com/musicplayer/ui/SplashActivity.kt
+++ app/src/main/java/com/musicplayer/ui/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.musicplayer.R
 
 class SplashActivity : AppCompatActivity() {
 
